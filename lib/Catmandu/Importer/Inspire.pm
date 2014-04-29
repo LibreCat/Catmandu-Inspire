4f2febc75aee74ebcc0345eee57a46aa6b18b666
vpeil <vitali.peil@uni-bielefeld.de>
2014-04-29 08:38:38 +0200

    test modified

--- lib/Catmandu/Importer/Inspire.pm
+++ lib/Catmandu/Importer/Inspire.pm
@@ -1,139 +1,101 @@
 package Catmandu::Importer::Inspire;
 
-use strict;
-use warnings;
 use Catmandu::Sane;
-use Moo;
-use Furl;
 use XML::LibXML::Simple qw(XMLin);
+use Furl;
+use Moo;
 
 with 'Catmandu::Importer';
 
-# INFO:
-# http://inspirehep.net/
-# at the moment no api available
-
-
-# Constants. -------------------------------------------------------------------
-
-use constant BASE_URL => 'http://inspirehep.net/';
+use constant BASE_URL       => 'http://inspirehep.net/';
 use constant DEFAULT_FORMAT => 'endnote';
 
-# Properties. ------------------------------------------------------------------
+has base   => ( is => 'ro', default => sub { return BASE_URL; } );
+has format => ( is => 'ro', default => sub { return DEFAULT_FORMAT; } );
+has doi    => ( is => 'ro' );
+has id     => ( is => 'ro' );
 
-# required.
-has base => (is => 'ro', default => sub { return BASE_URL; });
-has format => (is => 'ro', default => sub { return DEFAULT_FORMAT; });
-has doi => (is => 'ro');
-has id => (is => 'ro');
-
-# Mapping
 my %FORMAT_MAPPING = (
     'endnote' => 'xe',
-    'nlm' => 'xn',
-    'marc' => 'xm',
-    'dc' => 'xd',
-    );
-# Internal Methods. ------------------------------------------------------------
+    'nlm'     => 'xn',
+    'marc'    => 'xm',
+    'dc'      => 'xd',
+);
 
-# Internal: HTTP GET something.
-#
-# $url - the url.
-#
-# Returns the raw response object.
 sub _request {
-  my ($self, $url) = @_;
+    my ( $self, $url ) = @_;
 
-  my $furl = Furl->new(
-    agent => 'Mozilla/5.0',
-    timeout => 10
-  );
+    my $furl = Furl->new(
+        agent   => 'Mozilla/5.0',
+        timeout => 10
+    );
 
-  my $res = $furl->get($url);
-  die $res->status_line unless $res->is_success;
+    my $res = $furl->get($url);
+    die $res->status_line unless $res->is_success;
 
-  return $res;
+    return $res;
 }
 
-# Internal: Converts XML to a perl hash.
-#
-# $in - the raw XML input.
-#
-# Returns a hash representation of the given XML.
 sub _hashify {
-  my ($self, $in) = @_;
+    my ( $self, $in ) = @_;
 
-  my $xs = XML::LibXML::Simple->new();
-  my $out = $xs->XMLin(
-	  $in, 
-  );
+    my $xs  = XML::LibXML::Simple->new();
+    my $out = $xs->XMLin( $in, );
 
-  return $out;
+    return $out;
 }
 
-# Internal: Makes a call to the Inspire record
-#
-# Returns the XML response body.
 sub _call {
-  my ($self) = @_;
+    my ($self) = @_;
 
-  # construct the url
-  my $url = $self->base;
-  #my $query = $self->query || '';
-  #my $id = $self->id || '';
-  my $fmt = $FORMAT_MAPPING{$self->format};
-  if ($self->doi) {
-    #$url .= "search?&action_search=Suchen&of=" . $fmt . "p=" . $query;
-    $url .= 'search?p=doi%3A'. $self->doi .'&of='. $fmt .'&action_search=Suchen';
-  } elsif ($self->id) {
-    $url .= 'record/'. $self->id . '/export/' . $fmt;
-  } else {
-    Catmandu::BadVal->throw("Either ID or DOI is required.");
-  }
+    my $url = $self->base;
+    my $fmt = $FORMAT_MAPPING{ $self->format };
+    if ( $self->doi ) {
+        $url
+            .= 'search?p=doi%3A'
+            . $self->doi . '&of='
+            . $fmt
+            . '&action_search=Suchen';
+    }
+    elsif ( $self->id ) {
+        $url .= 'record/' . $self->id . '/export/' . $fmt;
+    }
+    else {
+        Catmandu::BadVal->throw("Either ID or DOI is required.");
+    }
 
-  # http get the url.
-  my $res = $self->_request($url);
+    my $res = $self->_request($url);
 
-  # return the response body.
-  return $res->{content};
+    return $res->{content};
 }
 
-# Internal: gets the next set of results.
-#
-# Returns a hash representation of the resultset.
 sub _get_record {
-  my ($self) = @_;
-  
-  # fetch the xml response and hashify it.
-  my $xml = $self->_call;
-  my $hash = $self->_hashify($xml);
+    my ($self) = @_;
 
-  # return a reference to a hash.
-  return $hash;
+    my $xml  = $self->_call;
+    my $hash = $self->_hashify($xml);
+
+    return $hash;
 }
-
-# Public Methods. --------------------------------------------------------------
 
 sub generator {
-  my ($self) = @_;
-  my $return = 1;
+    my ($self) = @_;
+    my $return = 1;
 
-  return sub {
-	# hack to make iterator stop.
-	if ($return) {
-		$return = 0;
-		return $self->_get_record;
-	}
-	return undef;
-  };
+    return sub {
+
+        # hack to make iterator stop.
+        if ($return) {
+            $return = 0;
+            return $self->_get_record;
+        }
+        return undef;
+    };
 }
-
-
-# PerlDoc. ---------------------------------------------------------------------
 
 =head1 NAME
 
-  Catmandu::Importer::Inspire - Package that imports Inspire data.
+  Catmandu::Importer::Inspire - Package that imports Inspire data http://inspirehep.net/.
 
 =head1 SYNOPSIS
 
@@ -162,7 +124,7 @@ sub generator {
 
 =head1 SEE ALSO
 
-L<Catmandu::Iterable>
+L<Catmandu::Iterable>, L<Catmandu::ArXiv>
 
 =cut
 
--- t/00-load.t
+++ t/00-load.t
@@ -1,15 +1,58 @@
 #!/usr/bin/env perl
 
 use strict;
-use warnings;
+use warnings FATAL => 'all';
 use Test::More;
 use Test::Exception;
 
 my $pkg;
+my $pkg_fix;
 BEGIN {
   $pkg = 'Catmandu::Importer::Inspire';
-  use_ok($pkg);
+  $pkg_fix = 'Catmandu::Fix::inspire_extract_id';
+  use_ok $pkg;
+  use_ok $pkg_fix
 }
-require_ok($pkg);
+require_ok $pkg;
+require_ok $pkg_fix;
 
-done_testing 2;
+my $fixer = Catmandu::Fix->new(fixes => ['inspire_extract_id()']);
+
+my $importer = $pkg->new(
+	format => 'marc', 
+	doi => '10.1088/1126-6708/2009/03/112',
+	);
+
+isa_ok($importer, $pkg);
+can_ok($importer, 'each');
+
+my $importer2 = $pkg->new(
+	format => 'marc', 
+	id => '811388',
+	);
+
+isa_ok($importer2, $pkg);
+can_ok($importer2, 'each');
+
+
+my $f = $fixer->fix($importer);
+my $f2 = $fixer->fix($importer2);
+
+my $data = {
+    'url' => 'http://inspirehep.net/record/811388',
+    'id' => '811388',
+    'cern' => {
+    	'url' => 'http://cds.cern.ch/record/1156806',
+        'id' => '1156806'
+    },
+    'arxiv' => {
+        'url' => 'http://arxiv.org/abs/0901.3094',
+        'id' => '0901.3094'
+        },
+    'doi' => '10.1088/1126-6708/2009/03/112',
+	};
+
+is_deeply $f->first, $data, "compare records";
+is_deeply $f2->first, $data, "compare records";
+
+done_testing 10;
